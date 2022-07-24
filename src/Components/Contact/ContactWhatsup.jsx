33fdf2adf9fb64c371454ed3052bc5c2fe940bf2
rewebdev24 <rohitdhakad101@gmail.com>
2022-07-24 19:49:35 +0530

    contact8

--- src/Components/Contact/ContactWhatsup.jsx
+++ src/Components/Contact/ContactWhatsup.jsx
@@ -1,21 +1,16 @@
 import React, { useRef } from 'react'
 
 const ContactWhatsup = ({setShow,show}) => {
-     const nameref = useRef()
      const msgref = useRef()
      function send_handle(e){
           e.preventDefault()
-          var win = window.open(`https://wa.me/${+91-8449864328}`);
+          var win = window.open(`https://api.whatsapp.com/send?phone=${+91-8449864328}&text=${msgref.current.value}`);
          win.focus();
          setShow(!show)
         }
   return (
     <div className={`whatsup ${show?'show_whatsapp':""}`}>
       <form  className="whatsupform" onSubmit={send_handle}>
-          <div className="form-control">
-               <label htmlFor="name">Name : </label>
-               <input ref={nameref} type="text" name="" id="name" />
-          </div>
           <div className="form-control">
                <label htmlFor="msg">Message : </label>
                <textarea ref={msgref} type="text" name="" id="msg" />
