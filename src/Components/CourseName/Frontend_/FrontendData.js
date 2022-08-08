9defa5f366d429b8fdc75076e3074f1a9ddfd746
rewebdev24 <rohitdhakad101@gmail.com>
2022-08-09 02:00:12 +0530

    Projects

--- src/Components/CourseName/Frontend_/FrontendData.js
+++ src/Components/CourseName/Frontend_/FrontendData.js
@@ -142,8 +142,8 @@ const FrontendData = [
        <p>So for Building a website that user just only one thing <b>"DAMMNNN THIS WEBSITE"</b> after seeing it, you can add some animaton by using js animation libraries or some websites where the code is already written you just have to copy paste (you know developer things)</p>
        <h4>Js animation libraries</h4>
        <ul>
-       <li>Three.js</li>
        <li>Anime.js</li>
+       <li>Three.js (3d Animation)</li>
        <li>Velocity.js</li>
      </ul>
        <h4>CSS animation</h4>
@@ -162,34 +162,34 @@ const FrontendData = [
      <h4>HTML/CSS/Js</h4>
      <ul>
        <li>
-         <span>Code with harry </span><a href="https://www.youtube.com/watch?v=6mbwJ2xhgzM&list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg">https://www.youtube.com/watch?v=6mbwJ2xhgzM&list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg</a>
+         <span>Code with harry </span><a href="https://www.youtube.com/watch?v=6mbwJ2xhgzM&list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
        </li>
        <li>
-         <span>Apni Kaksha </span><a href="https://www.youtube.com/watch?v=l1EssrLxt7E&list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n">https://www.youtube.com/watch?v=l1EssrLxt7E&list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n</a>
+         <span>Apni Kaksha </span><a href="https://www.youtube.com/watch?v=l1EssrLxt7E&list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
        </li>
      </ul>
      <h4>Js</h4>
      <ul>
        <li>
-         <span>Thappa Techniacl : </span> <a href="https://www.youtube.com/watch?v=uGRZ2CzkFUo&list=PLwGdqUZWnOp1hqyT6h7pY0RlXIIGlE5U0">https://www.youtube.com/watch?v=uGRZ2CzkFUo&list=PLwGdqUZWnOp1hqyT6h7pY0RlXIIGlE5U0</a>
+         <span>Thappa Techniacl : </span> <a href="https://www.youtube.com/watch?v=uGRZ2CzkFUo&list=PLwGdqUZWnOp1hqyT6h7pY0RlXIIGlE5U0"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
        </li>
      </ul>
      <h4>Advanced CSS</h4>
      <ul>
        <li>
-         <span>Thappa technical </span> <a href="https://www.youtube.com/watch?v=AFKnthgCpWQ&list=PLwGdqUZWnOp2JYAoNE_-7sSWcIeO1A-xi">https://www.youtube.com/watch?v=AFKnthgCpWQ&list=PLwGdqUZWnOp2JYAoNE_-7sSWcIeO1A-xi</a>
+         <span>Thappa technical </span> <a href="https://www.youtube.com/watch?v=AFKnthgCpWQ&list=PLwGdqUZWnOp2JYAoNE_-7sSWcIeO1A-xi"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
        </li>
        <li>
-         <span>Online Tutorials </span> <a href="https://www.youtube.com/results?search_query=online+tutorials+css">https://www.youtube.com/results?search_query=online+tutorials+css</a>
+         <span>Online Tutorials </span> <a href="https://www.youtube.com/results?search_query=online+tutorials+css"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
        </li>
      </ul>
      <h4>Advanced Js</h4>
      <ul>
        <li>
-         <span>Yahoo Baba</span> <a href="https://www.youtube.com/watch?v=IUFJzLlf2cE&list=PL0b6OzIxLPbzvz4j1N4J8zCY8mu3l29MG">https://www.youtube.com/watch?v=IUFJzLlf2cE&list=PL0b6OzIxLPbzvz4j1N4J8zCY8mu3l29MG</a>
+         <span>Yahoo Baba</span> <a href="https://www.youtube.com/watch?v=IUFJzLlf2cE&list=PL0b6OzIxLPbzvz4j1N4J8zCY8mu3l29MG"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
        </li>
        <li>
-         <span>Coding Addict</span> <a href="https://www.youtube.com/watch?v=80KX6aD9R7M&list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ">https://www.youtube.com/watch?v=80KX6aD9R7M&list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ</a>
+         <span>Coding Addict</span> <a href="https://www.youtube.com/watch?v=80KX6aD9R7M&list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
        </li>
      </ul>
     </div>`,
@@ -202,19 +202,19 @@ const FrontendData = [
     <h4>HTML/CSS : </h4>
     <ul>
       <li>
-       <span>W3 Schools</span> <a href="https://www.w3schools.com/html/" target="_blank">https://www.w3schools.com/html/</a>
+       <span>W3 Schools</span> <a href="https://www.w3schools.com/html/" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
       </li>
       <li>
-       <span>Geeks For Geeks</span> <a href="https://www.geeksforgeeks.org/html/" target="_blank">https://www.geeksforgeeks.org/html/</a>
+       <span>Geeks For Geeks</span> <a href="https://www.geeksforgeeks.org/html/" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
       </li>
     </ul>
     <h4>Java Script</h4>
     <ul>
       <li>
-        <span>MDN web Docs</span> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">https://developer.mozilla.org/en-US/docs/Web/JavaScript</a> 
+        <span>MDN web Docs</span> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a> 
       </li>
       <li>
-      <span>Digital Ocean </span><a href="https://www.digitalocean.com/community/tags/javascript" target="_blank">https://www.digitalocean.com/community/tags/javascript</a> 
+      <span>Digital Ocean </span><a href="https://www.digitalocean.com/community/tags/javascript" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i></a> 
       </li>
     </ul>
     <p>See, MDN is best, but for beginner it could be a little confusing, MDN explain each topic in depth so if you are a beginner don't try to read the whole documentataion just read the main point and go for the next topic</p>
@@ -228,19 +228,19 @@ const FrontendData = [
     <h4><i class="fa-solid fa-angles-right"></i> Free code Camp [Free]</h4>
       <ul>
         <li>
-          <span>HTML </span> <a href="https://www.freecodecamp.org/news/html-crash-course/">https://www.freecodecamp.org/news/html-crash-course/</a>
+          <span>HTML </span> <a href="https://www.freecodecamp.org/news/html-crash-course/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
         </li>
         <li>
-          <span>CSS </span> <a href="https://www.freecodecamp.org/learn/responsive-web-design/">https://www.freecodecamp.org/learn/responsive-web-design/</a>
+          <span>CSS </span> <a href="https://www.freecodecamp.org/learn/responsive-web-design/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
         </li>
         <li>
-          <span>Js </span> <a href="https://www.freecodecamp.org/news/learn-javascript-full-course/">https://www.freecodecamp.org/news/learn-javascript-full-course/</a>
+          <span>Js </span> <a href="https://www.freecodecamp.org/news/learn-javascript-full-course/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
         </li>
       </ul>
       <h4><i class="fa-solid fa-angles-right"></i> Udemy [paid]</h4>
       <ul>
         <li>
-          <span>HTML/CSS/Js</span> <a href="https://www.udemy.com/course/the-complete-web-development-bootcamp/?data_h=BUAecVdUTHQ%3D&deal_code=1812GIFTINGB&utm_source=adwords&utm_medium=udemyads&utm_campaign=DSA_Catchall_la.EN_cc.INDIA&utm_content=deal4584&utm_term=_._ag_82569850245_._ad_533220805577_._kw__._de_c_._dm__._pl__._ti_dsa-406594358574_._li_20454_._pd__._&matchtype=&gclid=Cj0KCQjwuO6WBhDLARIsAIdeyDIwG_aCZEESjoGls2ad5EgOO0wsYcY4ti1eyLDA2RG6-xm-LZ4bXosaAmigEALw_wcB">https://www.udemy.com/course/the-complete-web-development-bootcamp/?data_h=BUAecVdUTHQ%3D&deal_code=1812GIFTINGB&utm_source=adwords&utm_medium=udemyads&utm_campaign=DSA_Catchall_la.EN_cc.INDIA&utm_content=deal4584&utm_term=_._ag_82569850245_._ad_533220805577_._kw__._de_c_._dm__._pl__._ti_dsa-406594358574_._li_20454_._pd__._&matchtype=&gclid=Cj0KCQjwuO6WBhDLARIsAIdeyDIwG_aCZEESjoGls2ad5EgOO0wsYcY4ti1eyLDA2RG6-xm-LZ4bXosaAmigEALw_wcB</a>
+          <span>HTML/CSS/Js</span> <a href="https://www.udemy.com/course/the-complete-web-development-bootcamp/?data_h=BUAecVdUTHQ%3D&deal_code=1812GIFTINGB&utm_source=adwords&utm_medium=udemyads&utm_campaign=DSA_Catchall_la.EN_cc.INDIA&utm_content=deal4584&utm_term=_._ag_82569850245_._ad_533220805577_._kw__._de_c_._dm__._pl__._ti_dsa-406594358574_._li_20454_._pd__._&matchtype=&gclid=Cj0KCQjwuO6WBhDLARIsAIdeyDIwG_aCZEESjoGls2ad5EgOO0wsYcY4ti1eyLDA2RG6-xm-LZ4bXosaAmigEALw_wcB"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
         </li>
       </ul>
     </div>`,
@@ -248,15 +248,36 @@ const FrontendData = [
   {
     id: 5,
     name: "Projects",
-    data: ` <div className="data">
-    <h3><i class="fa-solid fa-angles-right"></i> Projects</h3>
+    data: `  <div className="data">
+    <h3>Step-by-Step guide for projects</h3>
+    <p style="margin-bottom: 0">So a basic website contains : </p>
+    <ul>
+      <li>Navbar</li>
+      <li>Landing page</li>
+      <li>Cards</li>
+      <li>Footer (containing Contact info , address etc...)</li>
+      <li>Modal (A pop up for signup/signin etc...) </li>
+    </ul>
+    <p>You should try to build all these components separately then you can add all these to build a website</p>
+    <h3>Navbar</h3>
+    <ul>
+      <li><p><a href="https://www.youtube.com/watch?v=gXkqy0b4M5g" target="_blank"></a>Dev Ed [Responsove Navbar] <i class="fa-solid fa-arrow-up-right-from-square"></i></p></li>
+      <li><p><a href="https://www.youtube.com/watch?v=tVgnhxb-ScE" target="_blank"></a>Thappa Technical [Responsove Navbar] <i class="fa-solid fa-arrow-up-right-from-square"></i></p></li>
+    </ul>
+    <h3>Landing Page</h3>
+    <p>First !! try it Yourself it's not that hard you just have to  put items at the center</p>
+    <ul>
+      <li><p><a href="https://www.youtube.com/watch?v=RaITAxEdTDY" target="_blank"></a>true Coder [Responsove Landing Page] <i class="fa-solid fa-arrow-up-right-from-square"></i></p></li>
+    </ul>
+    <p style="color:red">Other components will be uploaded soon...</p>
+    <h3>Other Projects</h3>
    <h4>HTML</h4>
    <ul>
     <li>
-      <p>Form - [scholarship form <a href="https://www.jotform.com/form-templates/application-form-scholarship-grant">https://www.jotform.com/form-templates/application-form-scholarship-grant</a> ]</p> 
+      <p>Form - [scholarship form <a href="https://www.jotform.com/form-templates/application-form-scholarship-grant"><i class="fa-solid fa-arrow-up-right-from-square"></i></a> ]</p> 
     </li>
     <li>
-      <p>Form - [bank appilcation form <a href="https://www.jotform.com/form-templates/application-form/banking-application-forms">https://www.jotform.com/form-templates/application-form/banking-application-forms</a> ]</p> 
+      <p>Form - [bank appilcation form <a href="https://www.jotform.com/form-templates/application-form/banking-application-forms"><i class="fa-solid fa-arrow-up-right-from-square"></i></a> ]</p> 
     </li>
    </ul>
    <h4>CSS</h4>
@@ -265,16 +286,16 @@ const FrontendData = [
        <span>First Style the Above forms using your own creativity</span> 
       </li>
       <li>
-       <span>Registration : </span><a href="https://www.youtube.com/watch?v=okbByPWS1Xc">https://www.youtube.com/watch?v=okbByPWS1Xc</a> 
+       <span>Registration1 : </span><a href="https://www.youtube.com/watch?v=okbByPWS1Xc"><i class="fa-solid fa-arrow-up-right-from-square"></i></a> 
       </li>
       <li>
-       <span>Registration : </span><a href="https://www.youtube.com/watch?v=p9qBEq-YRSc">https://www.youtube.com/watch?v=p9qBEq-YRSc</a> 
+       <span>Registration2 : </span><a href="https://www.youtube.com/watch?v=p9qBEq-YRSc"><i class="fa-solid fa-arrow-up-right-from-square"></i></a> 
       </li>
     </ul>
-    <h4>Js</h4>
+    <h4>For Mastery in Js</h4>
     <ul>
       <li>
-        <span>Free code Camp 40 projects [beginner to Advance]</span> <a href="https://www.freecodecamp.org/news/javascript-projects-for-beginners/">https://www.freecodecamp.org/news/javascript-projects-for-beginners/</a>
+        <span>Free code Camp 40 projects [beginner to Advance]</span> <a href="https://www.freecodecamp.org/news/javascript-projects-for-beginners/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
       </li>
     </ul>
     </div>
